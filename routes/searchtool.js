b3195d234c2d1b674cc491c5a93559f94328fe40
luca <wissel.luca@gmail.com>
2016-09-24 17:31:27 +0200

    Initial commit

---
+++ routes/searchtool.js
@@ -0,0 +1,35 @@
+/*
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+
+router.get('/searchtool', function(req, res){
+var experiments=[];
+    /!*var string = "<option value=\"\" selected=\"selected\" ></option>";
+    *!/mongoose.connection.on('open', function(ref){
+     experiments =mongoose.connection.db.listCollections().toArray(function(err, names) {
+         if (err) {
+             console.log(err);
+         }
+         else {
+             names.forEach(function(e,i,a) {
+                 /!*string= string+"<option value=\""+ e.name+"\" >"+e.name+"</option>"*!/
+                 experiments.push(e.name);
+             });
+         }
+         res.json(experiments);
+     });
+ });
+    /!*res.set('Content-Type', 'text/html');
+    res.send(new Buffer(string));
+*!/
+
+/!*    for(var i=0; i<experimentlist.length; i++){
+        console.log(experimentlist[i]);
+    }*!/
+
+
+})
+
+module.exports = router;*/
